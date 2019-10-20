60ef844ece2eab951cde5a69712ddbcd26887a8f
valen <valentun9@gmail.com>
2019-10-20 23:11:48 +0300

    added db seeder, geo-calc queries, filtering, select, sort, pagination and some course-related stuff

--- routes/bootcamps.js
+++ routes/bootcamps.js
@@ -4,10 +4,19 @@ const {
   getBootcamp,
   createBootcamp,
   updateBootcamp,
-  deleteBootcamp
+  deleteBootcamp,
+  getBootcampsInRadius
 } = require('../controllers/bootcamps');
 
+// Include other resource routers
+const courseRouter = require('./courses');
+
 const router = express.Router();
+
+// Re-route into other resourse routers
+router.use('/:bootcampId/courses', courseRouter);
+
+router.route('/radius/:zipcode/:distance').get(getBootcampsInRadius);
 
 router
   .route('/')
